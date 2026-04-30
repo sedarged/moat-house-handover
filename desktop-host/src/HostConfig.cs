fcd58d1ce6280aa7c1f6d121f9c3c2453a61f01e
Degra <kamil.side.hustle@gmail.com>
2026-04-30 06:38:30 +0100

    Add data root path config contract

--- desktop-host/src/HostConfig.cs
+++ desktop-host/src/HostConfig.cs
@@ -5,11 +5,23 @@ namespace MoatHouseHandover.Host;
 /// </summary>
 public sealed class HostConfig
 {
+    public required string DataRoot { get; init; }
+
     public required string AccessDatabasePath { get; init; }
+
+    public string? SQLiteDatabasePath { get; init; }
 
     public required string AttachmentsRoot { get; init; }
 
     public required string ReportsOutputRoot { get; init; }
 
-    public string? LogRoot { get; init; }
+    public required string BackupsRoot { get; init; }
+
+    public required string LogRoot { get; init; }
+
+    public required string ConfigRoot { get; init; }
+
+    public required string ImportsRoot { get; init; }
+
+    public required string MigrationRoot { get; init; }
 }
